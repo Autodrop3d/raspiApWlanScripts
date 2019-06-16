7df12f615faa1316089c933e06649696ae93aa9c
John Scimone <john@scimone.net>
2019-06-15 22:12:47 -0400

    updated setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
 # disable debian networking and dhcpcd
-sudo -Es
 systemctl mask networking.service
 systemctl mask dhcpcd.service
 mv /etc/network/interfaces /etc/network/interfaces~
@@ -9,3 +15,54 @@ sed -i '1i resolvconf=NO' /etc/resolvconf.conf
 systemctl enable systemd-networkd.service
 systemctl enable systemd-resolved.service
 ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+
+cat >/etc/wpa_supplicant/wpa_supplicant-wlan0.conf <<EOF
+country=DE
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+
+network={
+    ssid="TestNet"
+    psk="verySecretPwassword"
+}
+EOF
+
+chmod 600 /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+systemctl disable wpa_supplicant.service
+systemctl enable wpa_supplicant@wlan0.service
+
+cat > /etc/wpa_supplicant/wpa_supplicant-ap0.conf <<EOF
+country=DE
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+
+network={
+    ssid="RPiNet"
+    mode=2
+    key_mgmt=WPA-PSK
+    proto=RSN WPA
+    psk="anotherPassword"
+    frequency=2412
+}
+EOF
+
+chmod 600 /etc/wpa_supplicant/wpa_supplicant-ap0.conf
+
+cat > /etc/systemd/network/08-wlan0.network <<EOF
+[Match]
+Name=wlan0
+[Network]
+DHCP=yes
+EOF
+
+cat > /etc/systemd/network/12-ap0.network <<EOF
+[Match]
+Name=ap0
+[Network]
+Address=192.168.4.1/24
+DHCPServer=yes
+[DHCPServer]
+DNS=84.200.69.80 84.200.70.40
+EOF
+
+systemctl disable wpa_supplicant@ap0.service
